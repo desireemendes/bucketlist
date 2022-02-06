59c464cbd67055bf62f5badf632363cfb08d4359
Desiree Mendes <desireefrost@live.ca>
2022-02-05 20:20:12 -0500

    add info window onClick

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -5,8 +5,9 @@ import {
   InfoWindow,
 } from "@react-google-maps/api";
 import { formatRelative } from "date-fns";
-import mapStyles from "./mapStyles"
+import mapStyles from "./mapStyles";
 import { useState } from "react";
+import { useCallback, useRef } from "react";
 
 const libraries = ["places"];
 const mapContainerStyle = {
@@ -21,7 +22,7 @@ const options = {
   styles: mapStyles,
   disableDefaultUI: true,
   zoomControl: true,
-}
+};
 
 function Map() {
   const { isLoaded, loadError } = useLoadScript({
@@ -29,7 +30,24 @@ function Map() {
     libraries,
   });
 
-  const [markers, setMarkers] = useState([])
+  const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
+
+  const onMapClick = useCallback((event) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = useRef();
+  const onMapLoad = useCallback((map) => {
+    mapRef.current = map;
+  }, []);
 
   if (loadError) return "Error loading map";
   if (!isLoaded) return "Loading map";
@@ -41,28 +59,37 @@ function Map() {
         zoom={8}
         center={center}
         options={options}
-        onClick={ (event) => {
-          setMarkers(current => [...current, {
-          lat: event.latLng.lat(),
-          lng: event.latLng.lng(),
-          time: new Date(),
-        },
-       ])
-      }}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
-       {markers.map((marker => (
-         <Marker
-         key={marker.time.toISOstring()}
-         position={{ lat: marker.lat, lng: marker.lng}}
-         icon={{
-           url: "/plane.svg",
-           scaledSize: new window.google.maps.Size(30, 30),
-           origin: new window.google.maps.Point(0, 0),
-           anchor: new window.google.maps.Point(15, 15)
-         }}
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOstring()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            //  icon={{
+            //    url: "/plane.svg",
+            //    scaledSize: new window.google.maps.Size(30, 30),
+            //    origin: new window.google.maps.Point(0, 0),
+            //    anchor: new window.google.maps.Point(15, 15)
+            //  }}
+            onClick={() => {
+              setSelected(marker);
+            }}
           />
-       )))}
-
+        ))}
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Bucket List</h2>
+              <p>Added {formatRelative(selected.time, newDate())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
     </div>
   );
